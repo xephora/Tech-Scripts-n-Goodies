aada74905b1edaac910f07e6e4be5a44eb31ca57
Shawn <41995051+xephora@users.noreply.github.com>
2019-09-09 12:02:23 -0400

    Update URLParam.py

--- Python/URLParam.py
+++ Python/URLParam.py
@@ -1,8 +1,15 @@
 import urllib
 from urllib import request
 
-urlinput = input('Insert URL_ ExampleURL http://URL/PATH/query?key=')
+print('Insert URL_ ExampleURL http://URL/PATH/query?key=')
+urlinput = input()
+print('\n')
 
 resp = request.urlopen(urlinput)
 
+print('Size of HTTP Response is')
 print(resp.length)
+print('\n')
+
+print('Response Peek')
+print(resp.peek())
