7d04a9ca5b9966714bfa2cc309854abb3679db64
Shawn <41995051+xephora@users.noreply.github.com>
2019-11-27 22:07:29 -0500

    Update poke.py

--- SoLazyScriptz/toolk1t/1_domainscraping/domain_cleanup/poke.py
+++ SoLazyScriptz/toolk1t/1_domainscraping/domain_cleanup/poke.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-read_file = open('/root/pwn/toolk1t/1_domainscraping/domain_cleanup/domain_ips.txt','r')
+read_file = open('domain_ips.txt','r')
 for host in read_file:
 	os.system("sniper -t " + host)
 read_file.close()
