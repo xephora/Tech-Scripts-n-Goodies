16f3b2b735a09da638a2b23e344a08aba42e5113
Shawn <41995051+xephora@users.noreply.github.com>
2019-11-28 20:26:34 -0500

    Add files via upload

---
+++ SoLazyScriptz/toolk1t/3_Content-Discovery/Fuzzxor/ffufxor.rb
@@ -0,0 +1,49 @@
+puts <<-'EOF'                                                
+   __  __            __                          
+  69MM69MM          69MM                         
+ 6M' 6M' `         6M' `                         
+_MM__MM____   ___ _MM_____   ___  _____  ___  __ 
+MMMMMMMM`MM    MM MMMM`MM(   )P' 6MMMMMb `MM 6MM 
+ MM  MM  MM    MM  MM  `MM` ,P  6M'   `Mb MM69 " 
+ MM  MM  MM    MM  MM   `MM,P   MM     MM MM'    
+ MM  MM  MM    MM  MM    `MM.   MM     MM MM     
+ MM  MM  MM    MM  MM    d`MM.  MM     MM MM     
+ MM  MM  YM.   MM  MM   d' `MM. YM.   ,M9 MM     
+_MM__MM_  YMMM9MM__MM__d_  _)MM_ YMMMMM9 _MM_    
+                                                                                                                                                   
+EOF
+
+
+require "rubygems"
+require "highline/import"
+
+def option_1
+	system("clear")
+	puts("Loading Quickscan")
+	system("/root/pwn/toolk1t/3_Content-Discovery/Fuzzxor/ffuf_scripts/ffuf_quick")
+	exit
+end
+
+def option_2
+	system("clear")
+	puts("Loading Fullscan")
+	system("/root/pwn/toolk1t/3_Content-Discovery/Fuzzxor/ffuf_scripts/ffuf_full")
+	exit
+end
+
+def option_3
+	system("clear")
+	puts("Loading Most Interesting Scan MmMMmMmM")
+	system("ffuf_most_interesting")
+	exit
+end
+
+loop do
+	choose do |menu|
+	menu.prompt = "ffuf Scan options: "
+	menu.choice("1 ffuf quickscan") { option_1() }
+	menu.choice("2 ffuf fullscan") { option_2() }
+	menu.choice("3 ffuf most interesting") { option_3() }
+	menu.choice("0 Exit") { exit }
+	end
+end
