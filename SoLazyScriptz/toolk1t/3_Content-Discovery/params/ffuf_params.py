28f2a4df6ba784029d602d8a7b3219bce4c82fa5
Shawn <41995051+xephora@users.noreply.github.com>
2019-12-14 16:20:56 -0500

    Update ffuf_params.py

--- SoLazyScriptz/toolk1t/3_Content-Discovery/params/ffuf_params.py
+++ SoLazyScriptz/toolk1t/3_Content-Discovery/params/ffuf_params.py
@@ -5,7 +5,7 @@ website = raw_input('Enter URL Format - https://example.com/doc: ')
 
 read_file = open('/root/pwn/toolk1t/3_Content-Discovery/params/paramlist.txt','r')
 for param in read_file:
-	#For Debugger use print function
+	#For Debugging purposes use print function
 	#print("ffuf -c -w /root/pwn/toolk1t/3_Content-Discovery/params/param_wordlists/XSS-Jhaddix.txt --fc 403,404,302 -u " + website + "?" + param.rstrip() + "=FUZZ >> logs/" + param)
 	print('Checking for XSS huge list against ' + param)
         os.system("ffuf -c -w /root/pwn/toolk1t/3_Content-Discovery/params/param_wordlists/XSS-Cheat-Sheet-PortSwigger.txt --fc 403,404,302 -u " + website + "?" + param.rstrip() + "=FUZZ >> logs/" + param)
