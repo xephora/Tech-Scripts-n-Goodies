af73d4f94d0c8141248d41c4b3440b46e034bbc3
Shawn <41995051+xephora@users.noreply.github.com>
2020-02-02 21:32:20 -0500

    Update fuzz_rootfiles.py

--- SoLazyScriptz/toolk1t/3_Content-Discovery/Fuzzxor/fuzz_rootfiles.py
+++ SoLazyScriptz/toolk1t/3_Content-Discovery/Fuzzxor/fuzz_rootfiles.py
@@ -3,8 +3,8 @@ import os
 
 read_file = open('hitlist.txt','r')
 
-for mostint in read_file:
+for domains in read_file:
 	#debugger
 	#print("ffuf -c -w /root/wordlist/Fuzzing/curated.txt --fc 403,404,302 -u " + mostint.rstrip() + "/FUZZ")
-	os.system("ffuf -c -w wordlist_rootfiles.txt --fc 403,404,302 -u " + mostint.rstrip() + "/FUZZ")
+	os.system("ffuf -c -w wordlist_rootfiles.txt --fc 403,404,302 -u " + domains.rstrip() + "/FUZZ")
 read_file.close()
