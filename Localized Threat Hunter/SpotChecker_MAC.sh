d8a4c1db0377ace7d7bb453bd407f7ef4af2f6c7
Shawn <41995051+xephora@users.noreply.github.com>
2020-03-02 08:53:34 -0500

    Update SpotChecker_MAC.sh

--- Localized Threat Hunter/SpotChecker_MAC.sh
+++ Localized Threat Hunter/SpotChecker_MAC.sh
@@ -1,5 +1,8 @@
-ls -la /Library/startupItems >> StartupLocations.log 
-ls -la /System/Library/StartupItems >> StartupLocations.log 
+ls -lt /Library/startupItems >> StartupLocations.log 
+ls -lt /System/Library/StartupItems >> StartupLocations.log 
+ls -lt /System/Library/LaunchDaemons >> ServiceLocations.log
+ls -lt /Library/LaunchAgents >> ServiceLocations.log
+ls -lt /Applications >> Applications.log
 ps aux > Active_Processes.log 
 lsof > Listening_Ports.log 
 launchctl list > Service_list.log
