41e86bcd4d9863bcf7d00ec0bef310bc5ccebcb3
Shawn <41995051+xephora@users.noreply.github.com>
2020-03-13 14:53:37 -0400

    Update SpotChecker_MAC.sh

--- Localized Threat Hunter/SpotChecker_MAC.sh
+++ Localized Threat Hunter/SpotChecker_MAC.sh
@@ -1,8 +1,23 @@
-ls -lt /Library/startupItems >> StartupLocations.log 
-ls -lt /System/Library/StartupItems >> StartupLocations.log 
-ls -lt /System/Library/LaunchDaemons >> ServiceLocations.log
-ls -lt /Library/LaunchAgents >> ServiceLocations.log
-ls -lt /Applications >> Applications.log
+#Checks the common locations of malware, spyware and saves logs to the current location
+read -p 'Enter Username: ' user
+
+echo "System Files Inspected" >> InspectionLog.txt
+ls -lt /var/root >> InspectionLog.txt
+ls -lt /Library/Application Support >> InspectionLog.txt
+ls -lt /Library/LaunchDaemons >> InspectionLog.txt
+ls -lt /Library/LaunchAgents >> InspectionLog.txt
+echo "User based Files Inspected" >> InspectionLog.txt
+ls -lt /Users/$user/Library/LaunchAgents >> InspectionLog.txt
+ls -lt "/Users/$user/Library/Application Support" >> InspectionLog.txt
+ls -lt /Users/<username>/Library >> InspectionLog.txt
+echo "Startup Locations and Applications" >> InspectionLog.txt
+ls -lt /Library/startupItems >> InspectionLog.txt
+ls -lt /System/Library/StartupItems >> InspectionLog.txt
+ls -lt /System/Library/LaunchDaemons >> InspectionLog.txt
+ls -lt /Library/LaunchAgents >> InspectionLog.txt
+ls -lt /Applications >> InspectionLog.txt
+
+#check processes, Listening Ports and Services saves logs to current location
 ps aux > Active_Processes.log 
 lsof > Listening_Ports.log 
 launchctl list > Service_list.log
