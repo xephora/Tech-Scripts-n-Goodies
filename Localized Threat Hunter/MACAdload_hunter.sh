e89aab0e1ef388761836f765b28de00b0b3496f9
Shawn <41995051+xephora@users.noreply.github.com>
2020-05-21 22:35:10 -0400

    Create MACAdload_hunter.sh

---
+++ Localized Threat Hunter/MACAdload_hunter.sh
@@ -0,0 +1,44 @@
+cat <<EOF
+ __    __     ______     ______     ______     _____     __         ______     ______     _____            
+/\ "-./  \   /\  __ \   /\  ___\   /\  __ \   /\  __-.  /\ \       /\  __ \   /\  __ \   /\  __-.          
+\ \ \-./\ \  \ \  __ \  \ \ \____  \ \  __ \  \ \ \/\ \ \ \ \____  \ \ \/\ \  \ \  __ \  \ \ \/\ \         
+ \ \_\ \ \_\  \ \_\ \_\  \ \_____\  \ \_\ \_\  \ \____-  \ \_____\  \ \_____\  \ \_\ \_\  \ \____-         
+  \/_/  \/_/   \/_/\/_/   \/_____/   \/_/\/_/   \/____/   \/_____/   \/_____/   \/_/\/_/   \/____/         
+                                                                                                           
+                                           __  __     __  __     __   __     ______   ______     ______    
+                                          /\ \_\ \   /\ \/\ \   /\ "-.\ \   /\__  _\ /\  ___\   /\  == \   
+                                          \ \  __ \  \ \ \_\ \  \ \ \-.  \  \/_/\ \/ \ \  __\   \ \  __<   
+                                           \ \_\ \_\  \ \_____\  \ \_\\"\_\    \ \_\  \ \_____\  \ \_\ \_\ 
+                                            \/_/\/_/   \/_____/   \/_/ \/_/     \/_/   \/_____/   \/_/ /_/ 
+                                                                                                           
+EOF
+
+echo "======================================================================================"
+read -p 'Enter a username you want to inspect: ' user
+echo -e "\n"
+echo -e "==================================================================================="
+echo -e "Hunting for MACAdload: \n"
+find /var/root/*proxy* >> /tmp/MACAdload_hunt.log
+find /var/root/*Search* >> /tmp/MACAdload_hunt.log
+find "/Library/Application Support/*Search*" >> /tmp/MACAdload_hunt.log
+find /Library/LaunchDaemons/*Search* >> /tmp/MACAdload_hunt.log
+find /Library/LaunchAgents/*Search* >> /tmp/MACAdload_hunt.log
+find /Users/$user/Library/LaunchAgents/*Search* >> /tmp/MACAdload_hunt.log
+find "/Users/$user/Library/Application Support/*Search*" >> /tmp/MACAdload_hunt.log
+find /Users/$user/Library/*Search* >> /tmp/MACAdload_hunt.log
+find /var/at/tabs/$user >> /tmp/MACAdload_hunt.log
+echo -e "\n"
+echo -e "==================================================================================="
+echo -e "A log has been generated below:"
+ls /tmp/MACAdload_hunt.log
+echo -e "\n"
+echo -e "==================================================================================="
+read -p 'Enter Python Script filename (example SearchQuest.py or com.SearchQuest.py): ' py
+echo "Checking Processes for $py"
+ps aux | grep $py
+echo "Checking Port Activity for $py"
+lsof | grep $py
+
+#Additional but optional
+#Hunting tmp directory. Enable or Disable feature by removing or adding the #
+#find /tmp/*-*-*-*-*
